cbb058914fff39167affc4e51853f6dae0201531
Prapthi P <146919647+Prapthi-P@users.noreply.github.com>
2024-06-07 22:23:46 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# <h1> Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# <ol> 
+#     <li> Python </li>
+#     <li> SQL </li>
+#     <li> R </li>
+#  </ol>   
+#     
+
+# Some of the commonly used libraries used by Data Scientists include:
+# <ol>
+#     <li> NumPy </li>
+#     <li> Pandas </li>
+#     <li> skicit-learn </li>
+# </ol>
+# 
+
+# <table>
+#     <tr>
+#         <th> Data Science Tools </th>
+#         <th>  </th>
+#         <th>  </th> 
+#     </tr>
+#     <tr>
+#         <td>MongoDB</td>
+#         <td> </td>
+#         <td> </td>
+#     </tr>
+#     <tr>
+#         <td> Apache Kafka </td>
+#         <td> </td>
+#         <td> </td>
+#     </tr>
+#     <tr>
+#         <td> Github </td>
+#         <td> </td>
+#         <td> </td>
+#     </tr>
+#     
+# </table>
+
+# <h3> Below are a few examples of evaluating arithmetic expressions in Python </h3>
+# 
+# 
+# a=5, b=2 
+# c=a+b;
+# 
+
+# In[1]:
+
+
+#This is a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+x_min= 200
+x_hr= 200/60
+print (x_hr)
+
+
+# <b>Objectives: </b>
+# <ul> 
+#     <li> Popular languages for data science: Python, R, sql, scala, java, c++, Julia </li>
+#     <li> Dataset is structured collection of data </li>
+#     <li> Kernel is a computational engine that executes code contained in a notebook file </li>
+#     <li> Anaconda is a free and open src distributor for python and R  </li>
+# </ul>    
+
+# <h2>Author</h2>
+# Prapthi
+# 
+
+# In[ ]:
+
+
+
+
